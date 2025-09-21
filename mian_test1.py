16d85dcae6819c3cdbe74a6914052d195628711e
Colin <h_c_p@qq.com>
2025-09-21 19:15:32 +0800

    add some print in the mian_test1.py

---
+++ mian_test1.py
@@ -0,0 +1,20 @@
+"""
+@File ：mian_test1.py
+@usage: --- To test usage of git verssion control ---
+
+@Author ：Colin
+@Date ：2025/9/21 18:36
+"""
+
+
+a = 4;
+
+b = 4;
+if a == b:
+    print("a == b")
+elif a > b:
+    print("a > b")
+else:
+    print("a < b")
+
+#这是第一个文件
